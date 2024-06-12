a5057d9d68a062d59bdadc3b57a5d8372d686ee8
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-06-12 01:39:42 +0000

    editted admin/ public/ namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,36 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'adresses/index'
-    get 'adresses/edit'
-    get 'adresses/update'
-    get 'adresses/create'
-    get 'adresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/create'
-    get 'orders/confirm'
-    get 'orders/completed'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/confirm'
-    get 'customers/destroy'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  root to: "admin/items#new"
   
-  namespace :admin do
-    root to: "admin/sessions#new"
-    resources :items
-  end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  devise_for :customers
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  
+  namespace :admin do
+    root to: "homes#top"
+    resources :items
+    resources :customers
+    resources :genres
+    
+  end
+  
+  
+  scope module: :public do
+    
+    root to: "homes#top"
+    get 'homes/about'
+    resources :addresses
+    resources :orders
+    resources :cart_items
+    resources :customers
+    resources :items
+    
+  end
+ 
+ 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
