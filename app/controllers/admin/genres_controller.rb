4e4dda495617043e586f4c90a9fc7516e6a6d0fa
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-06-21 16:43:04 +0000

    editted admin/genre and admin/item controllers and views

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,40 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "ジャンルを作成しました"
+      redirect_to admin_genres_path
+    else
+      @genre = Genre.new
+      @genres = genre.all
+      render :index
+    end
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "変更が完了しました"
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
+
+
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,38 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
+    
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "新規商品を作成しました"
+      redirect_to item_path(@item.id)
+    else
+      @item = Item.new
+      render :new
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def index
+    @items = Item.all
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 
 end
