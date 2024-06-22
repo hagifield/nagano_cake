13f8111ae1cb627f89344dcc291d1bddea96bf9a
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-06-22 16:24:25 +0000

    create get_cake_image in Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   
   has_one_attached :image
   belongs_to :genre
+  
+  def get_cake_image(width,height)
+    unless imame.attached?
+      file_path = Rails.root.join('app/assets/images/sweets_shortcake')
+      image.attach(io: File.open(file_path),filename: 'sweets_shortcake', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
