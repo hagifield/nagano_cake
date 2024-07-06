d614688a314a1d8ace2c400d85a76d26272fc294
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-07-06 16:45:27 +0000

    installed enum and created devise.ja.yml, ja.yml and editted order and order_detail model to assosiate them

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_details, dependent: :destroy
+  enum order_status: { pending: 0, confirmed: 1, shipped: 2, delivered: 3 }
+  enum payment_method: { credit_card: 0, transfer: 1 }
 end
